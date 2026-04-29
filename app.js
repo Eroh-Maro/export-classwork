eb37f868c80eb053a4132a9731eee33f5541445a
Eroh Oghenemaro Divine <maroeroh@gmail.com>
2026-04-29 19:08:21 +0100

    submission

---
+++ app.js
@@ -0,0 +1,6 @@
+import message from "./message.js";
+import { multiply, divide } from "./calculator.js";
+
+multiply(8,2)
+divide(8,2)
+message()
---
+++ message.js
@@ -0,0 +1,5 @@
+function message(){
+   console.log("Hello Eroh Oghenemaro Divine")
+}
+
+export default message
